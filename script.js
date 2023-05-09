b5a239eb675c9d3ecc2dbaceffce256778b4b1fb
gabrielebarcellona <gabrerozabarcy5@gmail.com>
2023-05-09 17:44:04 +0200

    add structure style

--- script.js
+++ script.js
@@ -10,3 +10,33 @@ MILESTONE 2
 Visualizzare a fianco ad ogni item una "x": cliccando su di essa, il todo viene rimosso dalla lista.
 MILESTONE 3
 Predisporre un campo di input testuale e un pulsante "aggiungi": cliccando sul pulsante, il testo digitato viene letto e utilizzato per creare un nuovo todo, che quindi viene aggiunto alla lista dei todo esistenti.*/
+
+const app = Vue.CreateApp({
+    data(){
+        return{
+            todo1 : {
+                text : 'ciao sono pippo',
+                done : false
+            },
+            todo2 : {
+                text : 'ciao sono michele',
+                done : false
+            },
+
+            todo3 : {
+                text : 'ciao sono pincopallino',
+                done : false
+            },
+             todo4 : {
+                text : 'ciao sono enrico',
+                done : false
+            },
+            todo5 : {
+                text : 'ciao sono giorgio',
+                done : false
+            }
+        }
+    }
+})
+
+app.mount('.list-group');
